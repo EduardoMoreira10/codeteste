3a12565a5c0962ea5a16240c4fae3096714a2276
3C Plus CA <cplusca@pop-os.localdomain>
2023-07-11 14:45:09 -0300

    adicionando console.log

--- app.js
+++ app.js
@@ -1,1 +1,4 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
+console.log("aplicação está se conectando ao banco de dados")
+console.log("Eduardo fez commit")
+console.log("Enviando para o github")
